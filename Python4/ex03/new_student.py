73c27e9403845cd45232806bb5e6fe0a871514b0
Chuah Tse Yung <105269435+DoughnutsNCookies@users.noreply.github.com>
2023-09-21 07:06:47 +0000

    ex03

---
+++ Python4/ex03/new_student.py
@@ -0,0 +1,24 @@
+import random
+import string
+from dataclasses import dataclass, field
+
+
+def generate_id() -> str:
+    """Generates a random id"""
+    return "".join(random.choices(string.ascii_lowercase, k=15))
+
+
+@dataclass
+class Student:
+    """Student Class"""
+    name: str = ""
+    surname: str = ""
+    active: bool = True
+    login: str = ""
+    id: str = field(default_factory=generate_id)
+
+    def __post_init__(self):
+        """Post init method"""
+        self.name = self.name
+        self.surname = self.surname
+        self.login = (self.name[0] + self.surname)
