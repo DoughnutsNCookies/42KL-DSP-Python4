131bdf875198f7fbaf65587e715ae5c112e9424f
Chuah Tse Yung <105269435+DoughnutsNCookies@users.noreply.github.com>
2023-09-21 16:26:58 +0000

    eval

--- Python4/ex02/tester.py
+++ Python4/ex02/tester.py
@@ -16,3 +16,24 @@ def g():
 for i in range(3):
     f()
     g()
+
+
+@callLimit(0)
+def h():
+    print("h()")
+
+
+@callLimit(2)
+def j():
+    print("j()")
+
+
+@callLimit(1)
+def k():
+    print("k()")
+
+
+for i in range(2):
+    h()
+    j()
+    k()
